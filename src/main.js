c21ffe012598eede4f92718d86169e24808a5fa1
Mohana Devarasetty <mdevarasetty@osidigital.com>
2021-02-25 16:54:12 +0530

    removed unused library

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
-import Modal from "@burhanahmeed/vue-modal-2";
 import { HelloWorld } from  'dialogue-box';
 
 
 Vue.component('hello-world', HelloWorld)
-Vue.use(Modal)
 
 
 Vue.config.productionTip = false
